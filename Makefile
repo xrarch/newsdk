011ddab3f83c35b1c21ac843d8614bda78aa9d3b
hyenasky <ashnwill49@gmail.com>
2023-06-21 12:40:03 -0600

    Build: build system; Tower: placeholder for compiler

---
+++ Makefile
@@ -0,0 +1,38 @@
+# root makefile for the newsdk cross-build system
+
+export REPO := $(shell pwd)
+export BUILDROOT := $(REPO)/build
+
+ifndef SDK
+	SDK := $(REPO)/../sdk
+endif
+
+export SDK
+export TBC := $(SDK)/tbc.sh
+
+PROJECTS := Tower
+
+ifndef PROJECT
+	PROJECT := $(PROJECTS)
+endif
+
+all: $(PROJECT) | $(BUILDROOT)
+
+$(PROJECTS): FORCE
+	make -C $@
+
+FORCE: ;
+
+$(BUILDROOT):
+	mkdir -p $(BUILDROOT)
+
+cleanupall:
+	for platform in $(PLATFORMS); do \
+		make cleanup PLATFORM=$$platform; \
+		make cleanup PLATFORM=$$platform DEBUGCHECKS=1; \
+	done
+
+cleanup:
+	for dir in $(PROJECTS); do \
+		make -C $$dir cleanup; \
+	done
---
+++ Project.mk
@@ -0,0 +1,32 @@
+INCLUDES := $(subst :, ,$(INCDIR))
+INCLUDEFILES := $(foreach incdir,$(INCLUDES),$(shell find $(incdir) -type f -name "*.twh"))
+
+TWRFILES := $(foreach component,$(COMPONENTS),$(wildcard $(component)/*.twr)) \
+			$(foreach component,$(COMPONENTS),$(wildcard $(component)/host/*.twr))
+
+COBJ := $(TWRFILES:.twr=.c)
+OBJ  := $(TWRFILES:.twr=.o)
+
+FULLOUTPUTFILE = $(BUILDROOT)/$(OUTPUTFILE)
+
+all: $(FULLOUTPUTFILE)
+
+$(FULLOUTPUTFILE): $(OBJ)
+	$(CC) -o $(FULLOUTPUTFILE) $(OBJ)
+
+define COMPONENT_TEMPLATE
+
+$(1)/%.c: $(1)/%.twr $$(INCLUDEFILES) $$(wildcard $(1)/*.twh)
+	$$(TBC) $$< $$@ incdir=$$(INCDIR) libdir=$$(LIBDIR)
+
+$(1)/%.o: $(1)/%.c
+	$$(CC) $$< -c -o $$@
+
+endef
+
+$(foreach component,$(COMPONENTS), \
+	$(eval $(call COMPONENT_TEMPLATE,$(component))) \
+)
+
+cleanup:
+	rm -f ${COBJ} ${OBJ} $(FULLOUTPUTFILE)
