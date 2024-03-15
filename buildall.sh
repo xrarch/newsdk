77028737650f97238a5c2155c9e3fc8115ed8551
hyenasky <ashnwill49@gmail.com>
2024-03-15 06:56:49 -0600

    Meta: create buildall.sh

---
+++ buildall.sh
@@ -0,0 +1,3 @@
+./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 Jackal
+./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 XrBuildTool
+./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 XrAsm
