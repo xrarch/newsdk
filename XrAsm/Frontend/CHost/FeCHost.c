f5eb1df9a43ca1cafdc40f4dad7172441a98109c
hyenasky <ashnwill49@gmail.com>
2024-03-15 04:51:39 -0600

    Asm: frontend skeleton

---
+++ XrAsm/Frontend/CHost/FeCHost.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern void TlInitialize ();
+extern void AsmAssembleProgram (uint64_t argc, uint64_t argv);
+
+int main (int argc, char *argv[]) {
+    // Initialize the Toolchain Library.
+
+    TlInitialize ();
+
+    // Assemble the program.
+
+    AsmAssembleProgram ( (uint64_t)(argc), (uint64_t)(argv) );
+
+    // Return success.
+
+    return 0;
+}
+
+void LexStreamError_C (uint8_t* filename, uint64_t linenumber, uint64_t linepos, uint8_t* str, uint64_t err1, uint64_t err2, uint64_t err3) {
+    fprintf(
+        stderr,
+        "%s:%lld:%lld: %s (%lld %lld %lld)\n",
+        filename,
+        linenumber,
+        linepos,
+        str,
+        err1,
+        err2,
+        err3
+    );
+
+    exit(1);
+}
