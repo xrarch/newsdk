54e7b03c0b6925cb6d08d8475a7bc1dbfc7703e1
hyenasky <ashnwill49@gmail.com>
2024-05-22 05:43:17 -0600

    Lnk: fix bugs

--- buildall.sh
+++ buildall.sh
@@ -1,3 +1,4 @@
-./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 Jackal
-./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 XrBuildTool
-./bin/xrbt.exe ./build.xrbt ARCHITECTURE=CHost JOBS=$1 XrAsm
+./bin/xrbt.exe ./build.xrbt JOBS=$1 Jackal
+./bin/xrbt.exe ./build.xrbt JOBS=$1 XrBuildTool
+./bin/xrbt.exe ./build.xrbt JOBS=$1 XrAsm
+./bin/xrbt.exe ./build.xrbt JOBS=$1 XrLink
