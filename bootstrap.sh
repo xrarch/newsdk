1c869dfbbc8ac0dd12b13e8ee62bba74c98e9e12
hyenasky <ashnwill49@gmail.com>
2024-05-25 20:28:54 -0600

    Build: deduplicate library prebuilt

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 mkdir -p bin
 
 echo "Building initial jkl.exe"
-cc -x c -Wno-incompatible-library-redeclaration -Wno-builtin-requires-header -Ofast Jackal/Prebuilt/* -o bin/jkl.exe
+cc -x c -Wno-incompatible-library-redeclaration -Wno-builtin-requires-header -Ofast Jackal/Prebuilt/* Library/Prebuilt/* -o bin/jkl.exe
 
 echo "Building initial xrbt.exe"
-cc -x c -Wno-incompatible-library-redeclaration -Wno-builtin-requires-header -Ofast XrBuildTool/Prebuilt/* -o bin/xrbt.exe
+cc -x c -Wno-incompatible-library-redeclaration -Wno-builtin-requires-header -Ofast XrBuildTool/Prebuilt/* Library/Prebuilt/* -o bin/xrbt.exe
