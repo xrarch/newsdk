0300389cf14dab84526573298727916d04b0deb4
hyenasky <ashnwill49@gmail.com>
2024-05-25 19:59:24 -0600

    Bt: make build tool a lot more useful; Build: use useful new build tool stuff

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,4 @@
-./bin/xrbt.exe ./build.xrbt JOBS=$1 Jackal
-./bin/xrbt.exe ./build.xrbt JOBS=$1 XrBuildTool
-./bin/xrbt.exe ./build.xrbt JOBS=$1 XrAsm
-./bin/xrbt.exe ./build.xrbt JOBS=$1 XrLink
+./bin/xrbt.exe ./build.xrbt JOBS=$1 all
 
 cp build/CHost/jkl.exe bin/jkl.exe
 cp build/CHost/xrbt.exe bin/xrbt.exe
