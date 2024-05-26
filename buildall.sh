d505351dd0306d499b5633ee40bbaef617525928
hyenasky <ashnwill49@gmail.com>
2024-05-25 19:59:54 -0600

    Build: update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -4,3 +4,4 @@ cp build/CHost/jkl.exe bin/jkl.exe
 cp build/CHost/xrbt.exe bin/xrbt.exe
 cp build/CHost/xrasm.exe bin/xrasm.exe
 cp build/CHost/xrlink.exe bin/xrlink.exe
+cp build/CHost/xrdut.exe bin/xrdut.exe
